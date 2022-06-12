a2e83c188003f1f7ff0ebc92ef8c685c5656e5fa
johncaden124 <holland.caden.04@gmail.com>
2022-06-11 20:29:11 -0500

    this is my second commit

--- db/index.js
+++ db/index.js
@@ -1,11 +1,35 @@
-const pg = require('pg');
+const { Client } = require('pg'); // imports the pg module
 
-const client = new pg.Client(
-    "postgress://localhost:5432/petpals"
-);
+// supply the db name and location of the database
+const client = new Client('postgres://localhost:5432/juicebox-dev');
 
-client.connect();
-// console.log(client)
-module.exports = {
-    client
-};
+async function getAllUsers() {
+    const { rows } = await client.query(
+      `SELECT id, username 
+      FROM users;
+    `);
+  
+    return rows;
+  }
+
+  async function createUser({ username, password }) {
+    try {
+      const { rows } = await client.query(`
+        INSERT INTO users(username, password) 
+        VALUES($1, $2) 
+        ON CONFLICT (username) DO NOTHING 
+        RETURNING *;
+      `, [username, password]);
+  
+      return rows;
+    } catch (error) {
+      throw error;
+    }
+  }
+  
+  // and export them
+  module.exports = {
+    client,
+    getAllUsers,
+    createUser
+  }
